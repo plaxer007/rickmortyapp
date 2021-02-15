afac5d1511def558fe84f9058fc9dbc8aa2a15c5
plaxer007 <33569598+plaxer007@users.noreply.github.com>
2021-02-15 13:48:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+// API REQUEST
+
+const data = document.getElementById('data');
+const btn = document.querySelector('button');
+let charName, species, charStatus, gender;
+charName = document.querySelector('#name');
+species = document.querySelector('#species');
+charStatus = document.querySelector('#status');
+gender = document.querySelector('#gender');
+async function getCharacter(){
+    try{
+        // Type number of char you want to see
+        const number = prompt('type id number');
+
+        // Get char
+        const getSingleChar = await fetch(`https://rickandmortyapi.com/api/character/${number}`);
+        const parseChar = await getSingleChar.json();
+        const idChar = [
+            parseChar.name,
+            parseChar.status,
+            parseChar.species,
+            parseChar.gender,
+        ]
+        // Add char image
+        const img = document.getElementById('img');
+        const divs = document.querySelector('.box')
+        img.src = parseChar.image;
+        divs.append(img)
+        charName.textContent = parseChar.name;
+        if(parseChar.status === 'Alive'){
+            charStatus.textContent = 'OHHH yeaaah';
+        } else {
+            charStatus.textContent = 'Unfortunately not';
+        }
+        species.textContent = parseChar.species;
+        gender.textContent = parseChar.gender;
+        return idChar;
+    }
+    catch(e){
+        return console.log(e)
+    }
+}
+btn.addEventListener('click', getCharacter)
